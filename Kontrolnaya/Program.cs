c71a906b19e6e2045afffa4aaf01c0c2fbd8a341
AlexSilv <silverprof@mail.ru>
2022-08-05 00:15:29 +0300

    added kod

--- Kontrolnaya/Program.cs
+++ Kontrolnaya/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿string[] array1 = new string[5] {"hi", "Pavel", "thanks", "bye", ":-)"};
+string[] array2 = new string[array1.Length];
+void CreateArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+CreateArray(array1, array2);
+ShowArray(array2);
